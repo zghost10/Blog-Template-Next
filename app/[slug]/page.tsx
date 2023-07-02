177bfdd2d8159fcdb6711ecf258de06f02a4506d
zghost10 <rafael.karoso@gmail.com>
2023-07-02 06:26:11 -0300

    feat: implements post retrieving and post 404 warn

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@/components/container";
 import { getPostBySlug } from "@/lib/blog";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from 'next/link';
 
 interface IPostProps {
   params: {
@@ -7,19 +9,35 @@ interface IPostProps {
   }
 }
 
-export const Post: React.FC<IPostProps> = ({params}) => {
-  const content = getPostBySlug(params.slug);
+export const Post: React.FC<IPostProps> = async ({params}) => {
+  const post = await getPostBySlug(params.slug);
 
-  return <>
-    <title>Post title - Blog</title>
+  if(post){
+    const { frontmatter, content } = post;
+  
+    return <>
+      <title>{`${frontmatter?.title} - Blog`}</title>
+  
+      <Container>
+        <article className="prose lg:prose-xl dark:prose-invert">
+          <h3>{frontmatter?.title}</h3>
 
-    <Container>
-      <article className="prose lg:prose-xl dark:prose-invert">
-        <h3>Blog post</h3>
+          <MDXRemote source={content}/> 
+        </article>
+      </Container>
+    </>
+  }else{
+    return <>
+      <title>Post not found! - Blog</title>
 
-        Content
-      </article>
-    </Container>
-  </>
+      <Container>
+        <article className="prose lg:prose-xl dark:prose-invert">
+          <h3>404 - Post not found!</h3>
+
+          Try to write the post slug correctly or go to <Link href="/">blog home</Link> instead.
+        </article>
+      </Container>
+    </>
+  }
 }
 export default Post;
--- lib/blog.ts
+++ lib/blog.ts
@@ -1,5 +1,7 @@
 import { readFile } from "fs/promises";
 import { join } from "path";
+import matter from 'front-matter';
+import { IPost } from "@/types/blog";
 
 const POSTS_DIR_NAME = 'posts';
 const EXTENSION = '.mdx';
@@ -7,6 +9,14 @@ const postsDirectory = join(process.cwd(), POSTS_DIR_NAME);
 
 export const getPostBySlug = async (slug: string) => {
   const postPath = join(postsDirectory,slug+EXTENSION);
-  const postContent = await readFile(postPath,'utf-8').catch(() => false);
-  return postContent;
+  const post = await readFile(postPath,'utf-8')
+    .then(postFile => {
+      let { attributes, body } = matter(postFile);
+      return {
+        frontmatter: attributes as IPost,
+        content: body as string
+      }
+    })
+    .catch(() => console.log('Post not found!'));
+  return post;
 }
