49fd89fc18dd373c1fb126bafab942dff1d3e55d
zghost10 <rafael.karoso@gmail.com>
2023-07-02 07:39:56 -0300

    feat: simply adds a post list to blog home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
 import { Container } from "@/components/container";
+import { getPostList } from "@/lib/blog";
+import { IPost } from "@/types/blog";
 
-const Page = () => {
+const Page = async () => {
+  const posts = await getPostList();
+
+  const PostList = () => {
+    if(posts){
+      return posts.map((post, key) => {
+        if(post){
+          let { frontmatter } = post;
+          return (
+            <div key={key} className="flex flex-col py-4">
+              <h5>{frontmatter.title}</h5>
+              <h6>{frontmatter.description}</h6>
+            </div>
+          )
+        }
+      })
+    }else{
+      return <>no posts</>
+    }
+  }
+
   return <>
     <title>Home - Blog</title>
 
     <Container>
       <article className="prose lg:prose-xl dark:prose-invert">
         <h3>Blog home</h3>
+
+        <div className="grid grid-cols-1 divide-y">
+          <h4>Posts</h4>
+          <PostList/>
+        </div>
       </article>
     </Container>
   </>
--- lib/blog.ts
+++ lib/blog.ts
@@ -1,11 +1,29 @@
-import { readFile } from "fs/promises";
-import { join } from "path";
+import { readFile, readdir } from "fs/promises";
+import { extname, join } from "path";
 import matter from 'front-matter';
 import { IPost } from "@/types/blog";
+import { readdirSync } from "fs";
 
 const POSTS_DIR_NAME = 'posts';
 const EXTENSION = '.mdx';
 const postsDirectory = join(process.cwd(), POSTS_DIR_NAME);
+
+const removeExtension = (slug: string, extension = EXTENSION) => {
+  return slug.replace(extension,'');
+}
+
+const posts = readdirSync(postsDirectory).map(file => {
+  return removeExtension(file);
+});
+
+export const getPostList = async () => {
+  let list = posts.map(async slug => {
+    const post = await getPostBySlug(slug);
+    return post;
+  });
+  const postList = await Promise.all(list);
+  return postList;
+}
 
 export const getPostBySlug = async (slug: string) => {
   const postPath = join(postsDirectory,slug+EXTENSION);
@@ -17,6 +35,9 @@ export const getPostBySlug = async (slug: string) => {
         content: body as string
       }
     })
-    .catch(() => console.log('Post not found!'));
+    .catch(() => {
+      console.log('Post not found!');
+      return undefined;
+    });
   return post;
 }
